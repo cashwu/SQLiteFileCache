b8b9f1e2fce429045d7016711b3adca4948562dd
cash <cash@cashwu.com>
2020-08-26 15:41:44 +0800

    init

---
+++ testFileCache/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyCaching.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace testFileCache.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IEasyCachingProviderFactory _easyCachingProviderFactory;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IEasyCachingProviderFactory easyCachingProviderFactory)
+        {
+            _logger = logger;
+            _easyCachingProviderFactory = easyCachingProviderFactory;
+        }
+
+        [HttpGet]
+        public Test Get()
+        {
+            var cache = _easyCachingProviderFactory.GetCachingProvider("mm");
+
+            var test = cache.Get<Test>("test");
+
+            if (test.HasValue)
+            {
+                return test.Value;
+            }
+
+            var cacheValue = new Test
+            {
+                Now = DateTime.Now
+            };
+
+            cache.Set("test", cacheValue, TimeSpan.FromSeconds(10));
+
+            return cacheValue;
+        }
+    }
+
+    public class Test
+    {
+        public DateTime Now { get; set; }
+    }
+}
---
+++ testFileCache/Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyCaching.SQLite;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace testFileCache
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddEasyCaching(options =>
+            {
+                options.UseSQLite(config =>
+                {
+                    config.DBConfig = new SQLiteDBOptions
+                    {
+                        FileName = "my.db",
+                        CacheMode = SqliteCacheMode.Default,
+                        OpenMode = SqliteOpenMode.ReadWriteCreate
+                    };
+                }, "mm");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
